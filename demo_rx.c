bd9a22169dbf732894f48eadb822fa001411febf
Leipo Yan <leipo.yan@u-blox.com>
2016-09-23 03:07:07 -0700

    Updated demo_rx to have the option to dump to stdout or file.

--- demo_rx.c
+++ demo_rx.c
@@ -32,13 +32,12 @@ void intHandler(int signo) {
   exit(0);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
   dictionary *conf=NULL;
   int cport_nr=17,        /* /dev/ttyUSB1 */
       bdrate=115200;       /* 115200 baud */
   char mode[]={'8','N','1',0};
-  char outputFilename[256] = "out.out";
 
   unsigned char *buf=NULL;
   int bufSize=4*1024;
@@ -68,18 +67,24 @@ int main()
   cport_nr = iniparser_getint(conf, "rx:cport_nr", 17);
   bdrate = iniparser_getint(conf, "rx:bdrate", 115200);
   strncpy(mode, iniparser_getstring(conf, "rx:mode", "8N1"), 4);
-  strcpy(outputFilename, iniparser_getstring(conf, "rx:dumpfile", "out.out"));
   printf("cport is %d\n", cport_nr);
   printf("rate is %d\n", bdrate);
   printf("mode is %c %c %c %c\n", mode[0],mode[1],mode[2],mode[3]);
   iniparser_freedict(conf);
 
   /* Open output file to write */
-  fdw = open(outputFilename, O_WRONLY | O_CREAT, 0644);
-  if(fdw==0)
+  if(argc>1)
   {
-    printf("Failed to open %s\n", outputFilename);
-    return(0);
+    fdw = open(argv[1], O_WRONLY | O_CREAT, 0644);
+    if(fdw==0)
+    {
+      printf("Failed to open %s\n", argv[1]);
+      return(0);
+    }
+  }
+  else
+  {
+    fdw = 1;
   } 
 
   /* Open UART port */
@@ -98,7 +103,6 @@ int main()
     if(countr > 0)
     {
       write(fdw, buf, countr);
-      //write(1, buf, countr);
       //printf("received %i bytes. total received %i bytes.\n", countr, totalr);
     }
 
